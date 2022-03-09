c269591522b579081d35f72866058b4a506a5481
nilay <mailnilaydas@gmail.com>
2022-03-09 15:07:07 +0530

    added post method

--- src/main/java/com/example/myapp/controller/ProductController.java
+++ src/main/java/com/example/myapp/controller/ProductController.java
@@ -1,8 +1,10 @@
 package com.example.myapp.controller;
 
 import com.example.myapp.model.Product;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
@@ -11,8 +13,13 @@ import java.util.UUID;
 public class ProductController {
 
     @GetMapping("/products")
-    public ResponseEntity<Product> createProduct()
+    public ResponseEntity<Product> displayProduct()
     {
         return ResponseEntity.ok(Product.builder().productId(UUID.randomUUID().toString()).productName("Pen").price(20).build());
+    }
+    @PostMapping("/products")
+    public ResponseEntity<Product> createProduct(Product product)
+    {
+        return ResponseEntity.status(HttpStatus.CREATED).body(product);
     }
 }
